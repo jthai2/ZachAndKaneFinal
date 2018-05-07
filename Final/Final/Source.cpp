001663fb8eb08d944677eaec0bc0709ea1b42eae
Zach Hirst <32858964+zachroberthirst@users.noreply.github.com>
2018-05-07 12:41:39 -0500

    update

---
+++ Final/Final/Source.cpp
@@ -0,0 +1,41 @@
+// Authors Zach Hirst and Kane Greenwalt
+//5/4/18
+// Final 
+
+#include<iostream>
+#include<string> 
+#include<cstdlib>
+#include<time.h>
+#include<iomanip>
+
+using namespace std;
+
+int main()
+{
+	string levelchoice;
+
+	cout << " Welcome to Zach and Kane's C++ Final " << endl;
+	cout << "                                              " << endl;
+	cout << "     __         _____           _____         " << endl;
+	cout << "    |  |       [ O   ]         [ O   ]        " << endl;
+	cout << "    |  |      /[     ]        /[     ]        " << endl;
+	cout << "    |  |       [_____]         [_____]        " << endl;
+	cout << "    |__|       __| |___        __| |___       " << endl;
+	cout << "     ||        |       |      |       |       " << endl;
+	cout << "    / |        | |   | |      | |   | |       " << endl;
+	cout << " _ /__|___     | |   | |      | |   | |       " << endl;
+	cout << " |        |    |_|___|_|      |_|___|_|       " << endl;
+	cout << " |        |      |   |          |   |         " << endl;
+	cout << " |        |      | | |          | | |         " << endl;
+	cout << " | _______|      | | |          | | |         " << endl;
+	cout << " | |      |    __| | |__      __| | |__       " << endl; 
+	cout << " | | Dell |   |____|____|    |____|____|      " << endl;
+	cout << " |_|______|                                   " << endl;
+	cout << "                                              " << endl;
+	cout << " Select a level " << endl << " Level [1] - Easy \n Level [2] - Medium \n Level [3] - Hard \n Level [4] - Alien Survival " << endl;
+	cin >> levelchoice;
+	
+
+	system("Pause");
+	return 0;
+}
